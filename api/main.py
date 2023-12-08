fe2a040c7c22498c323c7266f8028ceec9704858
Felipe Lima <lima_felipe@rocketmail.com>
2023-12-08 16:11:17 +0100

    feat: add new endpoints

--- api/main.py
+++ api/main.py
@@ -8,7 +8,16 @@ async def root():
     return {'message': 'Hello World, workflow pipeline is working!'}
 
 @app.get('/test/')
-async def message():
+async def message1():
+    return {'message': 'Hello Lais, API is working!'}
+
+@app.get('/test/pt/')
+async def message2():
     return {'message': 'Hello Lais, API está funcionando!'}
+
+@app.get('/test/fr/')
+async def message3():
+    return {'message': 'Hello Lais, l\'API fonctionne!'}
+
 
 handler = Mangum(app=app)
