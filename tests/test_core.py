d9afd22596c35660e2bf807912fe317264dd110b
Felipe Lima <lima_felipe@rocketmail.com>
2023-12-08 13:49:50 +0100

    feat: change test message

--- tests/test_core.py
+++ tests/test_core.py
@@ -13,4 +13,4 @@ client = TestClient(app)
 def test_root():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Hello World'}
+    assert response.json() == {'message': 'Hello World, workflow pipeline is working!'}
